24a25c93904d106f48b46d22ddee44342c8a0be8
MB152655 <szx87121@student.szczecin.merito.pl>
2025-03-09 21:36:27 +0100

    zadanie1

--- zadanie_1/zadanie1.py
+++ zadanie_1/zadanie1.py
@@ -1,1 +1,21 @@
-print("Hello world!")
+import random  # Moduł do operacji losowych https://docs.python.org/3/library/random.html
+
+# Tworzenie dwóch list
+list1 = ['a', 'b', 'c', 'd']
+list2 = [1, 2, 3, 4]
+
+# Łączenie list funkcją zip() https://docs.python.org/3/library/functions.html#zip
+zipped_list = list(zip(list1, list2))
+print("Połączone listy:", zipped_list)
+
+# Wybór losowego elementu z połączonej listy (random.choice) https://docs.python.org/3/library/random.html#random.choice
+random_element = random.choice(zipped_list)
+print("Losowy element z połączonej listy:", random_element)
+
+# Obsługa wyjątku try-except https://docs.python.org/3/library/exceptions.html#ValueError
+try:
+    user_input = input("Podaj liczbę: ")
+    number = int(user_input)  # Próba konwersji inputu na liczbę całkowitą
+    print("Podana liczba to:", number)
+except ValueError as e:
+    print("Błąd: Podana wartość nie jest liczbą całkowitą!", e)
